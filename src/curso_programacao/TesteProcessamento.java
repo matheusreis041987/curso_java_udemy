4ae341308488df3ff59e16e5abee9d87041b9e08
matheusreis041987 <matheusgithub1987@gmail.com>
2024-09-15 19:33:57 -0300

    teste de processamento

---
+++ src/curso_programacao/TesteProcessamento.java
@@ -0,0 +1,37 @@
+package curso_programacao;
+
+public class TesteProcessamento {
+
+	public static void main(String[] args) {
+		int x, y;
+		x = 5;
+		y = 2 * x;
+		System.out.println(x);
+		System.out.println(y);
+		
+		double b,B,h,area;
+		
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		
+		area = (b + B) / 2.0 * h;
+		
+		System.out.println(area);
+		
+		int a, j;
+		double resultado;
+		a = 5;
+		j = 2;
+		
+		resultado = (double) a / j; // casting --> conversão dos valores
+
+		System.out.println(resultado);
+		
+		double t;
+		int r;
+		
+		
+	}
+
+}
