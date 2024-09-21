fd8f36ce48cd7b851fd00476bd7538ba21f20f80
matheusreis041987 <matheusgithub1987@gmail.com>
2024-09-21 17:48:43 -0300

    acerto no código

--- src/curso_programacao/CobrancaTelefonia.java
+++ src/curso_programacao/CobrancaTelefonia.java
@@ -16,7 +16,7 @@ public class CobrancaTelefonia {
 		double conta = 50.0;
 		
 		if (minutos > 100) {
-			conta = conta + (minutos - 100) * 2.0;
+			conta += (minutos - 100) * 2.0;
 		} 
 		
 		System.out.printf("Valor da conta = R$ %.2f%n", conta);
